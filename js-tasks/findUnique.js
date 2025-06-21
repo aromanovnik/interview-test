d362fe2efba2df5dfa669605db2abbc89e521fa5
Yerlan <E.Ashirbayev@gmail.com>
2025-06-21 18:59:43 +0500

    first task JavaScript done

--- js-tasks/findUnique.js
+++ js-tasks/findUnique.js
@@ -1,7 +1,27 @@
 const array = [1, 2, 5, 7, 7, 11, 12, 11, 1, 12];
 
 function findUnique(arr) {
-  // ...
+  dat = []
+  dat2 = []
+  dat3 = []
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (dat.includes(arr[i])) {
+      dat2.push(arr[i])
+    } else {
+      dat.push(arr[i])
+    }
+  }
+
+  for (let i = 0; i <= dat.length; i++) {
+    if (dat2.includes(dat[i])) {
+    } else if (dat[i] !== undefined) {
+      dat3.push(dat[i]);
+
+    }
+  }
+
+  console.log(dat3);
 }
 
 findUnique(array); // [2, 5]
