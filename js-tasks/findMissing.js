7a33c53a2fc733f73749ceb8ce520967db4f1b7e
konopyanovm <konopyanov.manap@gmail.com>
2025-07-11 11:09:24 +0500

    feat: added find unique function

--- js-tasks/findMissing.js
+++ js-tasks/findMissing.js
@@ -1,7 +1,7 @@
 const array = [1, 2, 3, 5];
 
 function findMissing(arr) {
-  const sortedArray = [...arr].sort((a,b) => a - b)
+  const sortedArray = [...arr].sort((a, b) => a - b)
 
   if(sortedArray[0] !== 1) return 1
 
--- js-tasks/findUnique.js
+++ js-tasks/findUnique.js
@@ -1,7 +1,21 @@
 const array = [1, 2, 5, 7, 7, 11, 12, 11, 1, 12];
 
 function findUnique(arr) {
-  // ...
+  const sortedArray = [...arr].sort((a, b) => a - b)
+  const result = []
+
+
+  for(let i = 1; i < sortedArray.length - 1; i++) {
+    const prevNumber = sortedArray[i - 1];
+    const currentNumber  = sortedArray[i];
+    const nextNumber  = sortedArray[i + 1];
+
+    if(prevNumber !== currentNumber && currentNumber !== nextNumber) {
+      result.push(currentNumber);
+    }
+  }
+
+  return result;
 }
 
-findUnique(array); // [2, 5]
+console.log(findUnique(array)); // [2, 5]
