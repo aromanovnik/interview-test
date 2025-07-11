907fd788f0500ac6ccf8d489cf7c57afc98a24fb
konopyanovm <konopyanov.manap@gmail.com>
2025-07-11 10:36:55 +0500

    feat: find missing function added

--- js-tasks/findMissing.js
+++ js-tasks/findMissing.js
@@ -1,7 +1,18 @@
-const array = [2, 1, 3, 5];
+const array = [1, 2, 3, 5];
 
 function findMissing(arr) {
-  // ...
+  const sortedArray = [...arr].sort((a,b) => a - b)
+
+  if(sortedArray[0] !== 1) return 1
+
+  for(let i = 0; i < sortedArray.length - 1; i++) {
+    const currentNumber = sortedArray[i]
+    const nextNumber = sortedArray[i + 1]
+
+    if(currentNumber + 1 !== nextNumber && nextNumber !== undefined) return currentNumber + 1
+  }
+
+  return null;
 }
 
-findMissing(array);
+console.log(findMissing(array));
